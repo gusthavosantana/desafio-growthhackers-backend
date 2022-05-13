8140486658d87a7c46e39522350e308f15078bcb
Gusthavo Santana <gusthavosantana@gmail.com>
2022-05-13 14:24:30 -0300

    Add others modules to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ProductModule } from './product/product.module';
+import { CategoryModule } from './category/category.module';
+import databaseConfig from './config/database/sequelize';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SequelizeModule.forRoot(databaseConfig),
+    ProductModule,
+    CategoryModule,
+  ],
 })
 export class AppModule {}
