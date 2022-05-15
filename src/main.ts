2da640019b291f9215f023ee9217f8e779be7572
Gusthavo Santana <gusthavosantana@gmail.com>
2022-05-15 00:02:43 -0300

    enable swagger and cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { enableSwagger } from './config/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+  app.enableCors();
+
+  enableSwagger(app);
 
   await app.listen(process.env.PORT || 3001);
 }
